463ab848843a9202d278ce86b9013c3f2f6c65f8
frea45 <110092585+frea45@users.noreply.github.com>
2022-11-06 10:57:45 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,16 +7,16 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
+API_ID = config("API_ID","", default=None, cast=int)
+API_HASH = config("API_HASH","", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
-AUTH_USERS = config("AUTH_USERS", default=None, cast=int)
-LOG_CHANNEL = config("LOG_CHANNEL", default=None)
+BOT_UN = config("BOT_UN","ir_convertbot", default=None)
+AUTH_USERS = config("AUTH_USERS","763990585", default=None, cast=int)
+LOG_CHANNEL = config("LOG_CHANNEL","-1001208480573", default=None)
 LOG_ID = config("LOG_ID", default=None)
-FORCESUB = config("FORCESUB", default=None)
+FORCESUB = config("FORCESUB","SeriesPlus1", default=None)
 FORCESUB_UN = config("FORCESUB_UN", default=None)
 ACCESS_CHANNEL = config("ACCESS_CHANNEL", default=None)
-MONGODB_URI = config("MONGODB_URI", default=None)
+MONGODB_URI = config("MONGODB_URI","mongodb+srv://abirhasan2005:abirhasan@cluster0.lb2tp.mongodb.net/cluster0?retryWrites=true&w=majority", default=None)
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
