d0895beaa6e757a5adeabe09ca0ca06c12cfe85f
frea45 <110092585+frea45@users.noreply.github.com>
2022-11-06 11:10:42 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,15 +7,15 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID","", default=None, cast=int)
-API_HASH = config("API_HASH","", default=None)
+API_ID = config("API_ID","3335796", default=None, cast=int)
+API_HASH = config("API_HASH","138b992a0e672e8346d8439c3f42ea78", default=None)
 BOT_TOKEN = config("BOT_TOKEN","5002292255:AAG3EmBHEaTPRxW8hZ797xuES-baLWm29Wo", default=None)
 BOT_UN = config("BOT_UN","ir_convertbot", default=None)
 AUTH_USERS = config("AUTH_USERS","763990585", default=None, cast=int)
 LOG_CHANNEL = config("LOG_CHANNEL","wplus1", default=None)
 LOG_ID = config("LOG_ID","-1001482606933", default=None)
-FORCESUB = config("FORCESUB","SeriesPlus1", default=None)
-FORCESUB_UN = config("FORCESUB_UN", default=None)
+FORCESUB = config("FORCESUB","-1001208480573", default=None)
+FORCESUB_UN = config("FORCESUB_UN","seriesplus1", default=None)
 ACCESS_CHANNEL = config("ACCESS_CHANNEL","default", default=None)
 MONGODB_URI = config("MONGODB_URI","mongodb+srv://abirhasan2005:abirhasan@cluster0.lb2tp.mongodb.net/cluster0?retryWrites=true&w=majority", default=None)
 
